ed14fb44ed9f3d1167b508a515a5ad802b16ca25
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-08-16 14:01:50 -0300

    Add files via upload

---
+++ sql/PopulatingClientes.sql
@@ -0,0 +1,17 @@
+use ozzatech;
+
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("João", "da Silva", "559.665.666.-63", "66.606.696-6", "joaosilva@gmail.com", "joaoSenha", true);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Pedro", "dos Santos", "559.553.535.-63", "57.377.880-9", "pedrosantos@gmail.com", "pedroSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Gabriel", "Aguilar Paiva", "553.425.098.-40", "59.399.199-9", "gabrielaguilar@gmail.com", "GabrielSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Lorenzo", "Oliveira Zimbres", "518.164.418.-05", "50.300.100-7", "lorenzooliveira@gmail.com", "lorenzoSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Anthony", "Rodrigo Caetano Teixeira", "599.765.432.-03", "54.302.190-7", "anthonyrodrigo@gmail.com", "anthonySenha", true);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Matheus", "Rodrigues da Silva", "191.161.131.-11", "51.101.191-1", "matheusrodrigues@gmail.com", "matheusSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Vinícius", "Gomes Silva", "292.262.232.-02", "52.322.292-2", "viniciusgomes@gmail.com", "viniciusSenha", true);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Eduardo", "Sinico Oliveira", "393.363.433.-33", "34.303.393-3", "eduardosinico@gmail.com", "eduardoSenha", true);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Felipe", "Luis Almeida", "444.744.434.-44", "44.304.440-4", "felipeluis@gmail.com", "felipeSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Manuel", "Gomes da Silva", "595.555.452.-53", "55.505.595-5", "manuelgomes@gmail.com", "manuelSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Leonardo", "Davinci VI", "669.666.466.-06", "66.606.696-6", "leonardodavinci@gmail.com", "leonardoSenha", true);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Vicente", "Leonardo Eustacchio Filho", "898.785.838.-88", "84.808.880-8", "vicenteleonardo@gmail.com", "vicenteSenha", true);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Rodrigo", "Teixeira", "589.767.737.-77", "99.992.990-9", "rodrigoteixeira@gmail.com", "rodrigoSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Arthur", "Bertuzzo Tavares", "509.700.402.-03", "50.300.100-8", "arthurbertuzzo@gmail.com", "arthurSenha", false);
+insert into clientes(nome, sobrenome, cpf, rg, email, senha, pessoa) values ("Fernando", "Rheder Batista Nascimento", "580.780.480.-03", "55.301.180-8", "fernandorheder@gmail.com", "fernandoSenha", false);
