925a32eaf3122ed104bf2f45663514ddffa7a718
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2023-08-16 20:21:41 -0300

    MySql data (#17)
    * Criação do banco d dados (e seu modelo relacional)
    
    * Add files via upload
    
    * Delete PopulatingClientes.sql
    
    * Add files via upload
    
    * Rename tablesCreation.txt to tablesCreation.sql
    
    * Update PopulatingClientes.sql
    
    correção da senha fora do padrão "GabrielSenha" -> "gabrielSenha"
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    ---------
    
    Co-authored-by: GPaiva <123770443+Paiva8859@users.noreply.github.com>

---
+++ sql/PopulatingProdutos.sql
@@ -0,0 +1,17 @@
+use ozzatech;
+
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Mouse Gamer X", 355.99, "Logitech", "Preto", "Novo", false, "SP", 3000);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Mouse Gamer Y", 185.99, "Positivo", "Cinza", "Usado", true, "MG", 1000);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Mouse Gamer Z", 295.99, "Lenovo", "Branco", "Redirecionado", false, "RJ", 5000);
+
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Teclado Gamer X", 455.99, "Logitech", "Preto", "Novo", false, "SP", 3000);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Teclado Gamer Y", 235.99, "Lenovo", "Cinza", "Usasdo", true, "MG", 1500);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Teclado Gamer Z", 377.99, "Positivo", "Branco", "Redirecionado", false, "RJ", 2700);
+
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Monitor Gamer X", 1855.99, "Logitech", "Preto", "Novo", false, "SP", 3000);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("monitor Gamer Y", 1235.99, "Lenovo", "Cinza", "Usasdo", true, "MG", 1500);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Monitor Gamer Z", 1677.99, "Positivo", "Branco", "Redirecionado", false, "RJ", 2700);
+
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Gabinete Gamer X", 455.99, "Logitech", "Preto", "Novo", false, "SP", 3000);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Gabinente Gamer Y", 135.99, "Lenovo", "Cinza", "Usasdo", true, "MG", 1500);
+insert into produtos(nome, preco, marca, cor, condicao, origem, localizacao, quantidade) values ("Gabinete Gamer Z", 477.99, "Positivo", "Branco", "Redirecionado", false, "RJ", 2700);
