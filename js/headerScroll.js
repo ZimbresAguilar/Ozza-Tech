c6bce16deec710f676c69663e017f1ea12c6108a
anthonyrct <123770471+anthonyrct@users.noreply.github.com>
2023-08-18 19:31:39 +0000

    houveram mudanças, mas ainda faltam algumas coisas

--- js/headerScroll.js
+++ js/headerScroll.js
@@ -13,3 +13,20 @@ function leiaMais() {
         btnLeiaMais.innerHTML = "Leia Menos";
     }
 }/*---local do script do botão leia Mais, onde quando o usuario clicar no botão, o texto irá se expandir, falta uma trasição suave para baixo para acabar---*/
+
+function scrollToTop(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Ativa a rolagem suave
+      });
+    }
+
+    // Mostra o botão de rolagem ao rolar a página
+    window.onscroll = function() {
+      var scrollButton = document.querySelector('.scroll-top-button');
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollButton.style.display = 'null';
+      } else {
+        scrollButton.style.display = 'none';
+      }
+}
