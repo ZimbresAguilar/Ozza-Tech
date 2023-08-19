00dcf286286f57d6bcc030d261c62a8856b756f2
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-08-19 20:48:58 -0300

    Revert "LLZBranch -> TZCBranch (#38)"
    This reverts commit fca49da48b829d825c01c2ecb3f81a1c1a9d26ef.

--- js/carroselCards.js
+++ js/carroselCards.js
@@ -1,63 +1,69 @@
-// Pegando todos .carrossel-contanter
-const carrosselContainers = document.querySelectorAll(".carrossel-conteudo");
+const carroselvisivel = document.querySelector(".carrosel-visivel");
+const carrosel = carroselvisivel.querySelector(".carrosel");
+const prevButton = document.querySelectorAll(".prvItem");
+const nextButton = document.querySelectorAll(".nxtItem");
 
-// Iterando por cada .carrossel-container
-carrosselContainers.forEach(carrosselContainer => {
-    const carrosselVisivel = carrosselContainer.querySelector(".carrosel-visivel");
-    const carrosel = carrosselVisivel.querySelector(".carrosel");
-    const prevButton = carrosselContainer.querySelector(".prvItem");
-    const nextButton = carrosselContainer.querySelector(".nxtItem");
+let currentIndex = 0;
+
+function moveToNextItem() {
+    const card = carrosel.querySelector(".card").offsetWidth;
+    const containerWidth = carroselvisivel.offsetWidth;
+    const maxVisibleIndex = Math.floor(carrosel.scrollWidth / card) - Math.floor(containerWidth / card);
     
-    let currentIndex = 0;
-    
-    function moveToNextItem() {
-        const card = carrosel.querySelector(".card").offsetWidth;
-        const containerWidth = carrosselVisivel.offsetWidth;
-        const maxVisibleIndex = Math.floor(carrosel.scrollWidth / card) - Math.floor(containerWidth / card);
-        
-        if (currentIndex < maxVisibleIndex) {
-            currentIndex++;
-            carrosel.style.transform = `translateX(-${(currentIndex * card) * 1.08}px)`;
-        }
-        checkLastItem();
+    if (currentIndex < maxVisibleIndex) {
+        currentIndex++;
+        carrosel.style.transform = `translateX(-${(currentIndex * card) * 1.08}px)`;
     }
-    
-    function moveToPrevItem() {
-        const card = carrosel.querySelector(".card").offsetWidth;
-        
-        if (currentIndex > 0) {
-            currentIndex--;
-            carrosel.style.transform = `translateX(-${(currentIndex * card) * 1.08}px)`;
-        }
-        checkLastItem();
-    }
-    
-    function checkLastItem() {
-        const card = carrosel.querySelector(".card").offsetWidth;
-        const containerWidth = carrosselVisivel.offsetWidth;
-        const visibleItems = Math.floor(containerWidth / card);
-        const maxVisibleIndex = Math.floor(carrosel.scrollWidth / card) - visibleItems;
-    
-        if (currentIndex >= maxVisibleIndex) {
-            nextButton.classList.add("disabled");
-            nextButton.disabled = true;
-        } else {
-            nextButton.classList.remove("disabled");
-            nextButton.disabled = false;
-        }
-    
-        if (currentIndex === 0) {
-            prevButton.classList.add("disabled");
-            prevButton.disabled = true;
-        } else {
-            prevButton.classList.remove("disabled");
-            prevButton.disabled = false;
-        }
-    }
-    
-    nextButton.addEventListener("click", moveToNextItem);
-    prevButton.addEventListener("click", moveToPrevItem);
-    
-    // Verificar estado inicial do carrossel
     checkLastItem();
+}
+
+function moveToPrevItem() {
+    const card = carrosel.querySelector(".card").offsetWidth;
+    
+    if (currentIndex > 0) {
+        currentIndex--;
+        carrosel.style.transform = `translateX(-${(currentIndex * card) * 1.08}px)`;
+    }
+    checkLastItem();
+}
+
+function checkLastItem() {
+    const card = carrosel.querySelector(".card").offsetWidth;
+    const containerWidth = carroselvisivel.offsetWidth;
+    const visibleItems = Math.floor(containerWidth / card);
+    const maxVisibleIndex = Math.floor(carrosel.scrollWidth / card) - visibleItems;
+
+    if (currentIndex >= maxVisibleIndex) {
+        nextButton.forEach(button => {
+            button.classList.add("disabled");
+            button.disabled = true;
+        });
+    } else {
+        nextButton.forEach(button => {
+            button.classList.remove("disabled");
+            button.disabled = false;
+        });
+    }
+
+    if (currentIndex === 0) {
+        prevButton.forEach(button => {
+            button.classList.add("disabled");
+            button.disabled = true;
+        });
+    } else {
+        prevButton.forEach(button => {
+            button.classList.remove("disabled");
+            button.disabled = false;
+        });
+    }
+}
+
+nextButton.forEach(button => {
+    button.addEventListener("click", moveToNextItem);
 });
+prevButton.forEach(button => {
+    button.addEventListener("click", moveToPrevItem);
+});
+
+// Verificar estado inicial do carrossel
+checkLastItem();
