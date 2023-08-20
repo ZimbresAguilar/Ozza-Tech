1bf4dc0506e78118097e4d8df7dba83837d0cd8c
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-08-20 00:00:44 -0300

    Setting PHP PDO INDEX LOGIN

---
+++ pages/index/carossel-lancamentos.php
@@ -0,0 +1,74 @@
+<section class="container-lancamentos">
+                <div class="carrosel-cabecalho">
+                    <h2>Lançamentos</h2>
+                </div>
+
+                <div class="carrossel-conteudo">
+                    <div class="carrosel-btn">
+                        <button type="button" class="prvItem"> < </button>
+                        <button type="button" class="nxtItem"> > </button>
+                    </div>
+    
+                    <div class="carrosel-visivel">
+                        <div class="carrosel">
+    
+                        <?php foreach($lancamentos as $item):?>
+                            <div class="card">
+                                <div class="categoria">
+
+                                    <figure class="categoria-container">
+                                        <img decoding="async" src="assets/imagens/produtos/<?=$item->getImagem()?>" alt="Imagem de exemplo" class="imagem-produto">
+
+                                        <div class="fav-share">
+                                            <figure class="share-container">
+                                                <img src="assets/icones/share.png" alt="" class="filled-share">
+                                            </figure>
+                                            
+                                            <figure class="fav-container">
+                                                <img src="assets/icones/heart.png" alt="" class="hollow-heart">
+                                                
+                                                <img src="assets/icones/heart-fill.png" alt="" class="filled-heart">
+                                            </figure>
+                                        </div>
+                                    </figure>
+    
+                                    <div class="categoria-conteudo">
+                                        <div class="nome-container">
+                                            <h2 class="nome"> <?=$item->getNome()?> </h2>
+                                        </div> 
+
+                                        <div class="preco">
+                                            <div class="preco-antes">
+                                                <p>De R$ <?=($item->getPreco() + 100.00)?> por:</p>
+                                            </div>
+
+                                            <div class="preço-final">
+                                                <p class="preco-depois">R$<?=$item->getPrecoFormatado()?></p>
+                                                <span class="vista">À vista</span>
+                                            </div>
+
+                                            <div class="pix">
+                                                <p>No PIX com 15% de desconto</p>
+                                            </div>
+                                        </div>
+                                    </div>
+    
+                                    <div class="compras-button">
+                                    <button type="submit" class="comprar-button">
+                                            <h2>COMPRAR</h2>
+                                            <div class="comprar-button-hover-color"></div>
+                                        </button>
+
+                                        <figure class="categoria-button">
+                                            <a href="">
+                                                <img src="assets/icones/1413908.png" alt="">
+                                            </a>
+                                        </figure>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
---
+++ pages/universal/header.php
@@ -0,0 +1,56 @@
+<header>
+        <div class="header-top">
+            <div class="header-top-left"></div>
+
+            <nav>
+                <ul class="nav-content">
+                    <li class="nav-item">
+                        <a href="/index.php">HOME</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#">SOBRE</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#">CATEGORIAS</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="#">OFERTAS</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div class="header-top-right">
+                <figure class="site-theme-btn-container">
+                    <a href="">
+                        <img src="/assets/icones/dark_theme_regular_icon_204499.png" alt="">
+                    </a>
+                </figure>
+
+                <figure class="shopping-kart-btn-container">
+                    <a href="">
+                        <img src="/assets/icones/1413908.png" alt="carrinho de compras">
+                    </a>
+                </figure>
+
+                <figure class="user-account-btn-container">
+                    <a href="/pages/login.php">
+                        <img src="/assets/icones/202-2024792_user-profile-icon-png-download-fa-user-circle.png" alt="">
+                    </a>
+                </figure>
+            </div>
+        </div>
+
+        <div id="header-bottom" class="header-bottom">
+            <form action="" method="post" class="search-bar-form">
+                <fieldset class="search-bar">
+                    <input type="text" name="search-bar-content" class="search-bar-content" id="search-bar-content"
+                        placeholder="Digite aqui...">
+                    <input type="submit" value="" name="search-bar-submit" class="search-bar-submit"
+                        id="search-bar-submit">
+                </fieldset>
+            </form>
+        </div>
+    </header>
