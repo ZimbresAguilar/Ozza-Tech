9d4606b068a2e5cca0058bbb7648deda062b300f
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2023-08-22 16:00:00 -0300

    <title> da index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>OzzaTech</title>
     <!--LINKS-->
     <!--Google Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
